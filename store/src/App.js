9c6951ab0b7d0e98c8865b18cbd9c676f0c6be5a
India Moore <indiaslove@icloud.com>
2022-10-27 01:21:12 -0500

    react-js-cart

---
+++ store/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar';
+import { Nav } from 'react-bootstrap';
+
+function App() {
+  return (
+    <NavbarComponent>
+
+    </NavbarComponent>
+  );
+}
+
+export default App;
---
+++ store/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import {Button, Container, Navbar, Modal} from 'react-bootstrap'
+
+function NavbarComponent() {
+
+    return (
+        <Navbar expand="sm">
+            <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                <Button>Cart 0 Items</Button>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default NavbarComponent;
